a5a11b4d88b5ee9a9042e9cad7b20065a7d38757
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-08 00:22:04 +0300

    Update DetailFragment.kt

--- app/src/main/java/com/veg/kotlincase/view/detail/DetailFragment.kt
+++ app/src/main/java/com/veg/kotlincase/view/detail/DetailFragment.kt
@@ -10,33 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.veg.kotlincase.databinding.FragmentDetailBinding
 import com.veg.kotlincase.viewmodel.DetailViewModel
 
-//Doğru olan kısım burası
-/*
-class DetailFragment : Fragment() {
-    private lateinit var binding:FragmentDetailBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentDetailBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.lifecycleOwner=this
-
-        val selectedCharacter=DetailFragmentArgs.fromBundle(requireArguments()).selectedCharacter
-        val viewModelFactory=DetailViewModelFactory(selectedCharacter,requireActivity().application)
-
-        binding.detailViewModel=ViewModelProvider(this,viewModelFactory)[DetailViewModel::class.java]
-
-    }
-
-}
-*/
 class DetailFragment : DialogFragment() {
     private lateinit var binding:FragmentDetailBinding
 
