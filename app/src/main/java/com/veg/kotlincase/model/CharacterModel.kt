20d29bfc95a6fdc4032d979979870f2712586ed4
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2023-11-28 01:17:36 +0300

    Update CharacterModel.kt

--- app/src/main/java/com/veg/kotlincase/model/CharacterModel.kt
+++ app/src/main/java/com/veg/kotlincase/model/CharacterModel.kt
@@ -1,23 +1,13 @@
 package com.veg.kotlincase.model
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
-@Entity
 @Parcelize
 data class CharacterModel (
-        @PrimaryKey(autoGenerate = true)
-        @ColumnInfo(name="id")
         var id:Int?,
-        @ColumnInfo(name="character_name")
         val name:String?,
-        @ColumnInfo(name="character_username")
         val username:String?,
-        @ColumnInfo(name="character_email")
         val email:String?,
-        @ColumnInfo(name="character_phone")
         val phone:String?,
         ): Parcelable
